9321a437710aac79410260dff143ad3a19707de9
Julien Freche <jfreche@aldebaran-robotics.com>
2012-11-06 18:33:35 +0100

    Converter: test now launch the behavior
    Change-Id: I2718b16d483a2ebd40ad6044ca0babbfb145083f

--- python/converter/tests/test_all.sh
+++ python/converter/tests/test_all.sh
@@ -1,14 +1,31 @@
 #!/bin/sh
 
+logFile=test_result.log
+
 rm -Rf objects
 IFS=:
-for str in `find ../../../../../behaviors/ -name 'behavior.xar' -printf "%p:"`
+for str in `find ../../../../../ -name 'behavior.xar' -printf "%p:"`
 do
   echo "$str"
-  ../xar_converter.py "$str"
+  ../xar_converter.py "$str" >> $logFile
   if [ $? -ne 0 ]
   then
+    echo "Error while converting $str"
     exit
   fi
+  cd objects
+  timeout 10 python2 main.py >> $logFile
+  result=$?
+  if [ $result -ne 0 ]
+  then
+    if [ $result -eq 124 ]
+    then
+      echo "Warning: behavior $str has timed out"
+    else
+      echo "Error while executing $str"
+      exit
+    fi
+  fi
+  cd ..
   rm -Rf objects
 done
