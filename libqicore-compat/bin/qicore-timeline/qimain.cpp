9d0df3c3483a93d12da3a1ac8b21123a500b0ad0
cbegue <cbegue@aldebaran-robotics.com>
2013-05-21 09:38:23 +0200

    QiCore-Compat : Add qicore-timeline program for execute a animation.
    Change-Id: Iad3f1eb410c028417162a6852c76f137e28dfc0b
    Reviewed-on: http://gerrit.aldebaran.lan:8080/18016
    Tested-by: cbegue <cbegue@aldebaran-robotics.com>
    Reviewed-by: cbegue <cbegue@aldebaran-robotics.com>

---
+++ libqicore-compat/bin/qicore-timeline/qimain.cpp
@@ -0,0 +1,108 @@
+/*
+**  Copyright (C) 2012 Aldebaran Robotics
+**  See COPYING for the license
+*/
+
+#include <qicore-compat/model/animationmodel.hpp>
+#include <qicore-compat/timeline.hpp>
+
+#include <qimessaging/session.hpp>
+#include <qi/application.hpp>
+
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
+
+int main(int argc, char **argv)
+{
+  qi::Application a(argc, argv);
+  std::string masterAddress;
+  std::string animation_path;
+  //declare the program options
+  po::options_description desc("Usage :\n  qicore-timeline path/to/animation.anim [masterAddress] [options]\nOptions");
+  desc.add_options()
+      ("help", "Print this help.")
+      ("animation", "Path to animation executed")
+      ("master-address",
+       po::value<std::string>()->default_value(std::string("tcp://127.0.0.1:9559")),
+       "The master address");
+
+  po::positional_options_description pos;
+  pos.add("animation", 1);
+  pos.add("master-address", 2);
+
+  // parse and store
+  po::variables_map vm;
+  try
+  {
+    po::store(po::command_line_parser(argc, argv).
+        options(desc).positional(pos).run(), vm);
+    po::notify(vm);
+
+    if(vm.count("help"))
+    {
+      std::cout << desc << std::endl;
+      return 0;
+    }
+
+    if(vm.count("master-address") == 1 && vm.count("animation") == 1)
+    {
+      masterAddress = vm["master-address"].as<std::string>();
+      animation_path = vm["animation"].as<std::string>();
+    }
+    else
+    {
+      std::cout << desc << std::endl;
+      return 0;
+    }
+
+  }
+  catch(const boost::program_options::error&)
+  {
+    std::cout << desc << std::endl;
+    return 0;
+  }
+
+  qi::Session session;
+  try
+  {
+    session.connect(masterAddress);
+  }
+  catch(qi::FutureUserException e)
+  {
+    std::cerr << "Connection error : " << e.what() << std::endl;
+    return 1;
+  }
+
+  qi::ObjectPtr memory;
+  qi::ObjectPtr motion;
+  try
+  {
+    memory = session.service("ALMemory");
+    motion = session.service("ALMotion");
+  }
+  catch(qi::FutureUserException e)
+  {
+    std::cerr << e.what() <<std::endl;
+    session.close();
+    return 1;
+  }
+
+
+  qi::Timeline timeline(memory, motion);
+  qi::AnimationModelPtr anim(new qi::AnimationModel(animation_path));
+
+  if(!anim->loadFromFile())
+  {
+    session.close();
+    return 0;
+  }
+
+  timeline.setAnimation(anim);
+  timeline.play();
+  timeline.waitForTimelineCompletion();
+
+  session.close();
+
+  return 0;
+}
