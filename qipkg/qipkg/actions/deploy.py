0a4f46ee86eeb21543847d716ebd59619e17e4a7
Dimitri Merejkowsky <dmerejkowsky@aldebaran-robotics.com>
2014-05-07 14:22:08 +0200

    qipkg: fix deploy
    Change-Id: Ifc98c1f5a1d404f344cdccaa320bd743826d95f4

--- qipkg/qipkg/actions/deploy.py
+++ qipkg/qipkg/actions/deploy.py
@@ -10,15 +10,21 @@ import os
 from qisys import ui
 import qisys.sh
 import qisys.parsers
+import qipkg.parsers
 import qipkg.package
 
 
 def configure_parser(parser):
     """Configure parser for this action"""
     qisys.parsers.deploy_parser(parser)
+    qipkg.parsers.pml_parser(parser)
 
 
 
 def do(args):
     """Main entry point"""
     urls = qisys.parsers.get_deploy_urls(args)
+    pml_builder = qipkg.parsers.get_pml_builder(args)
+    pml_builder.install(pml_builder.stage_path)
+    for url in urls:
+        pml_builder.deploy(url)
