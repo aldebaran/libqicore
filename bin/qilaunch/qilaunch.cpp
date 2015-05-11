86696b9614e94c29cc097105f0c07349d367b2b0
Herve CUCHE <hcuche@aldebaran.com>
2015-05-11 10:49:54 +0200

    Change qilaunch error logs to be less confusing
      * change --name by --log-category-name
    
    Change-Id: I415c547cf18b6f707b7fa0bbbd51c6383ac73452
    Reviewed-on: http://gerrit.aldebaran.lan/57658
    Approved: hcuche <hcuche@aldebaran.com>
    Reviewed-by: hcuche <hcuche@aldebaran.com>
    Tested-by: hcuche <hcuche@aldebaran.com>

--- bin/qilaunch/qilaunch.cpp
+++ bin/qilaunch/qilaunch.cpp
@@ -13,12 +13,14 @@ static bool disableBreakpad = false;
 static bool disableLogging = false;
 static std::string launcherName;
 
+qiLogCategory("qilaunch");
+
 _QI_COMMAND_LINE_OPTIONS(
   "Launcher options",
   ("module,m", value<std::vector<std::string> >(&modules), "Load given module (can be set multiple times)")
   ("no-breakpad,b", bool_switch(&disableBreakpad), "Disable breakpad")
   ("no-logging,l", bool_switch(&disableLogging), "Disable remote logging")
-  ("name,n", value<std::string>(&launcherName), "Launcher name")
+  ("name,n", value<std::string>(&launcherName), "Name of the launcher used to prefix logs and breakpad dump files")
 )
 
 int main(int argc, char** argv)
@@ -27,17 +29,16 @@ int main(int argc, char** argv)
 
   if (launcherName.empty())
   {
-    qiLogFatal("qilaunch") << "No launcher name, set one with --name";
+    qiLogFatal() << "No launcher name, set one with --name or -n";
     return 1;
   }
 
   if (modules.empty())
   {
-    qiLogFatal("qilaunch") << "No module to load, add one with --module";
+    qiLogFatal() << "No module to load, add one with --module or -m";
     return 1;
   }
 
-  qiLogCategory("qilaunch." + launcherName);
 
 #ifdef WITH_BREAKPAD
   boost::scoped_ptr<BreakpadExceptionHandler> eh;
@@ -56,7 +57,7 @@ int main(int argc, char** argv)
     try
     {
       if (!disableLogging)
-        qi::initializeLogging(app.session());
+        qi::initializeLogging(app.session(), launcherName);
     }
     catch (std::exception &e)
     {
