427a6326f66f01ff5172a8a81704fc9139971d17
Jerome Millet <jmillet@softbankrobotics.com>
2019-02-01 14:49:44 +0100

    QiCore is Registered on qicore.register() #43565
    Change-Id: Id93fa621500a005b314b66dabb266e3b8f9ff346
    Reviewed-on: http://gerrit.aldebaran.lan/93980
    Reviewed-by: jmillet <jmillet@aldebaran.com>
    Tested-by: jmillet <jmillet@aldebaran.com>

--- python-qicore/qicore/__init__.py
+++ python-qicore/qicore/__init__.py
@@ -8,19 +8,19 @@ from __future__ import print_function
 import os
 import sys
 
-# Get the Absolute Path if the Package
-path_package = os.path.dirname(os.path.realpath(__file__))
-
-# Get the Root Path of QiCore
-path_qicore = os.path.join(path_package, "linux")
-if "darwin" in sys.platform:
-    path_qicore = os.path.join(path_package, "mac")
-elif "win" in sys.platform:
-    path_qicore = os.path.join(path_package, "win")
-
-# Update QiAdditionalSdkPrefixes if needed
-qisdk_prefixes = os.environ.get("QI_ADDITIONAL_SDK_PREFIXES", "")
-if path_qicore not in qisdk_prefixes:
-    if qisdk_prefixes:
-        qisdk_prefixes += os.path.pathsep
-    os.environ["QI_ADDITIONAL_SDK_PREFIXES"] = qisdk_prefixes + path_qicore
+def register():
+    """ Register QiCore Python Module """
+    # Get the Absolute Path if the Package
+    path_package = os.path.dirname(os.path.realpath(__file__))
+    # Get the Root Path of QiCore
+    path_qicore = os.path.join(path_package, "linux")
+    if "darwin" in sys.platform:
+        path_qicore = os.path.join(path_package, "mac")
+    elif "win" in sys.platform:
+        path_qicore = os.path.join(path_package, "win")
+    # Update QiAdditionalSdkPrefixes if needed
+    qisdk_prefixes = os.environ.get("QI_ADDITIONAL_SDK_PREFIXES", "")
+    if path_qicore not in qisdk_prefixes:
+        if qisdk_prefixes:
+            qisdk_prefixes += os.path.pathsep
+        os.environ["QI_ADDITIONAL_SDK_PREFIXES"] = qisdk_prefixes + path_qicore
