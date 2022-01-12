b026de20dd90e5bc23f941e8bfdfcdd817a46fea
Vincent Palancher <vincent.palancher@softbankrobotics.com>
2022-01-12 19:30:39 +0100

    qicore: Removes use of libqi deprecated features.
    Change-Id: I5be0bf11f4ceaa58d1bf6c16b8ad0743b9405dd6
    Reviewed-on: http://gerrit2.aldebaran.lan/1771
    Reviewed-by: gerrit <gerrit@aldebaran-robotics.com>
    Reviewed-by: philippe.martin <philippe.martin@softbankrobotics.com>
    Reviewed-by: jmonnon <jmonnon@aldebaran.com>
    Tested-by: vincent.palancher <vincent.palancher@softbankrobotics.com>

--- libqicore/src/logproviderimpl.cpp
+++ libqicore/src/logproviderimpl.cpp
@@ -110,7 +110,7 @@ LogProviderImpl::LogProviderImpl()
   silenceQiCategories(_subscriber);
   ++_ready;
   sendTask.setName("LogProvider");
-  sendTask.setUsPeriod(100 * 1000); // 100ms
+  sendTask.setPeriod(qi::MilliSeconds(100));
   sendTask.setCallback(&LogProviderImpl::sendLogs, this);
   sendTask.start();
 }
@@ -126,7 +126,7 @@ LogProviderImpl::LogProviderImpl(LogManagerPtr logger)
   silenceQiCategories(_subscriber);
   ++_ready;
   sendTask.setName("LogProvider");
-  sendTask.setUsPeriod(100 * 1000); // 100ms
+  sendTask.setPeriod(qi::MilliSeconds(100));
   sendTask.setCallback(&LogProviderImpl::sendLogs, this);
   sendTask.start();
 }
